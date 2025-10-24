963ee22e4d814858784032591d30a63d4cff1a17
J-Dog <j-dog@j-dog.net>
2025-10-23 19:17:56 -0700

    rename xchain explorer apis to xchain platform apis

--- src/content/js/swagger-initializer.js
+++ src/content/js/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "/json/xchain-explorer-api.json",
+    url: "/json/xchain-platform-api.json",
     dom_id: '#swagger-ui',
     validatorUrl: null, // or undefined    
     deepLinking: true,

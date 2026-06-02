56ec55dcb6d41edb6336916bb04f512a859815f6
J-Dog <j-dog@j-dog.net>
2026-06-02 09:15:47 -0700

    Set protocol addresses in per-coin configs (BTC/LTC/DOGE)
    Populate burn/gas/protocol/community/explorer across mainnet/testnet/regtest
    from the generated address set (protocol=donate1, community=donate2,
    explorer=donate3).

--- src/configs/DOGE.js
+++ src/configs/DOGE.js
@@ -38,25 +38,25 @@ module.exports = {
         // Set network specific addresses
         switch(network){
             case 'mainnet':
-                address['burn']      = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Coin BURN address
-                address['gas']       = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Coin GAS address 
-                address['protocol']  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Protocol Donation address
-                address['community'] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Community Donation address
-                address['explorer']  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Explorer Donation address
+                address['burn']      = "DChainBurnAddressXXXXXXXXXXXawc9pt"; // Coin BURN address
+                address['gas']       = "DGasfpttCnTijuuoAdiJ9sXJjG7vQ5pMkW"; // Coin GAS address 
+                address['protocol']  = "DDonate1RBcwGnCRNnVtwuCmQyWW1Gn25f"; // Protocol Donation address
+                address['community'] = "DDonate2o3Sg4phybp92oFpkmv8S9ZhGSV"; // Community Donation address
+                address['explorer']  = "DDonate3FCoUgi1bxW5r9c2p75uKTLw9qE"; // Explorer Donation address
                 break;
             case 'testnet':
-                address['burn']      = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Coin BURN address
-                address['gas']       = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Coin GAS address 
-                address['protocol']  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Protocol Donation address
-                address['community'] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Community Donation address
-                address['explorer']  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Explorer Donation address
+                address['burn']      = "nchainburnaddressXXXXXXXXXXXYKgF7W"; // Coin BURN address
+                address['gas']       = "ngasn6zHFzJ72zpk3DBKmXhD2XtszujSDW"; // Coin GAS address 
+                address['protocol']  = "ndonate1dE87UXUFf4gjyhPg7hfQRJXVXr"; // Protocol Donation address
+                address['community'] = "ndonate2wev8vKDgvd1DHhtJtvkRbn2usJ"; // Community Donation address
+                address['explorer']  = "ndonate3xHD56SnmmSxbjX7UMSPfN7XmVA"; // Explorer Donation address
                 break;
             case 'regtest':
-                address['burn']      = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Coin BURN address
-                address['gas']       = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Coin GAS address 
-                address['protocol']  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Protocol Donation address
-                address['community'] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Community Donation address
-                address['explorer']  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Explorer Donation address
+                address['burn']      = "mvs8WdppEhzQLxfcYwrr1eoKA2nUFi55ff"; // Coin BURN address
+                address['gas']       = "mgasDTdKu5DsbW97qSRnE8raAuYpKMfmhg"; // Coin GAS address 
+                address['protocol']  = "mzdg8wGxgP3Jk45FuZPspumCL3Ruup37ob"; // Protocol Donation address
+                address['community'] = "mmXU8RU7q3BUsyT66rtw1H6P7B2ZZd9c5Y"; // Community Donation address
+                address['explorer']  = "n1AvTJLLSA1NHamHd5KFj9mRn6BEcwnVbf"; // Explorer Donation address
                 break;
         }
         config['address'] = address;
--- src/configs/LTC.js
+++ src/configs/LTC.js
@@ -38,25 +38,25 @@ module.exports = {
         // Set network specific addresses
         switch(network){
             case 'mainnet':
-                address['burn']      = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Coin BURN address
-                address['gas']       = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Coin GAS address 
-                address['protocol']  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Protocol Donation address
-                address['community'] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Community Donation address
-                address['explorer']  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Explorer Donation address
+                address['burn']      = "LXChainBurnAddressXXXXXXXXXXSkrYkJ"; // Coin BURN address
+                address['gas']       = "LXChainCN6yjHVqqS9tYzYVYZ8CCZcSx72"; // Coin GAS address 
+                address['protocol']  = "Ldonate18tNZcVThKm5MX33EjvhaanJ6Mg"; // Protocol Donation address
+                address['community'] = "Ldonate2io846q2e7q8dUArh3TNnaq9ENb"; // Community Donation address
+                address['explorer']  = "Ldonate3FfyqbYQAYxo3qjFLcu28oUdAfn"; // Explorer Donation address
                 break;
             case 'testnet':
-                address['burn']      = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Coin BURN address
-                address['gas']       = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Coin GAS address 
-                address['protocol']  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Protocol Donation address
-                address['community'] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Community Donation address
-                address['explorer']  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Explorer Donation address
+                address['burn']      = "mxchainburnaddressXXXXXXXXXXa8EAfp"; // Coin BURN address
+                address['gas']       = "mgashLN9oSvj2CUJYKWdNxh6VkamPg1Ges"; // Coin GAS address 
+                address['protocol']  = "mybp5CceJvVV5tNCCiF7oBiZWko2fNkmnT"; // Protocol Donation address
+                address['community'] = "muKEjejjXQvLY7Lp7Ecpn29gM2TCb5BLTF"; // Community Donation address
+                address['explorer']  = "mzCXcxcECbY5aNSXsfWjzKQN1YwoefEcG8"; // Explorer Donation address
                 break;
             case 'regtest':
-                address['burn']      = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Coin BURN address
-                address['gas']       = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Coin GAS address 
-                address['protocol']  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Protocol Donation address
-                address['community'] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Community Donation address
-                address['explorer']  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Explorer Donation address
+                address['burn']      = "mxchainburnaddressXXXXXXXXXXa8EAfp"; // Coin BURN address
+                address['gas']       = "mgas5QYE38Bg34hwEjFKaE7Gs536FARue4"; // Coin GAS address 
+                address['protocol']  = "mgNY2ZXbnNEkRT5ZRF8yGamivrSX2QH97h"; // Protocol Donation address
+                address['community'] = "n2DLJPppXUi8jC6fLiSkthZi2sc9UKiZHd"; // Community Donation address
+                address['explorer']  = "myL7sZGPEG3LhFXn7RFCZ321r8bxgmgDBz"; // Explorer Donation address
                 break;
         }
         config['address'] = address;
